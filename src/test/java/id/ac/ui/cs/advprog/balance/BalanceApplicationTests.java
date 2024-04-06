286616e462a9ff2d7410a6e9d33eafc633f09b64
hilaldfzn <hilalfauzan9@gmail.com>
2024-04-06 21:07:35 +0700

    first commit

---
+++ src/test/java/id/ac/ui/cs/advprog/balance/BalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
