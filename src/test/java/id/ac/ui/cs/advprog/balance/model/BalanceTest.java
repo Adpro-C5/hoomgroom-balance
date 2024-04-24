24957dbbd9f5bda5f800ce1d80c80ca213857213
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-04-24 20:18:28 +0700

    [RED] Fix tests for balance model

--- src/test/java/id/ac/ui/cs/advprog/balance/model/BalanceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/balance/model/BalanceTest.java
@@ -3,6 +3,8 @@ package id.ac.ui.cs.advprog.balance.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BalanceTest {
@@ -12,17 +14,17 @@ public class BalanceTest {
     @BeforeEach
     void setUp() {
         this.balance = new Balance();
-        this.balance.setBalanceId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        this.balance.setBalanceAmount(50000);
+        this.balance.setId(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"));
+        this.balance.setAmount(50000.0);
     }
 
     @Test
     void testGetBalanceId() {
-        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.balance.getBalanceId());
+        assertEquals(UUID.fromString("eb558e9f-1c39-460e-8860-71af6af63bd6"), this.balance.getId());
     }
 
     @Test
     void testGetBalanceAmount() {
-        assertEquals(50000, this.balance.getBalanceAmount());
+        assertEquals(50000, this.balance.getAmount());
     }
 }
